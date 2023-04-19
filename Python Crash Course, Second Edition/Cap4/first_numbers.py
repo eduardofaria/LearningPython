5688c4a59f62134e11ce180417e3bd94e4e2c1a3
Eduardo Faria <eduardofaria.rj@gmail.com>
2023-04-19 09:59:51 -0300

    range()

---
+++ Python Crash Course, Second Edition/Cap4/first_numbers.py
@@ -0,0 +1,18 @@
+for value in range(1, 3):
+    print(value)
+
+print("\n ---------------------------- \n")
+
+numbers = list(range(1, 5))
+print(numbers)
+
+print("\n ---------------------------- \n")
+
+impares = list(range(1, 20, 2))
+print("Ímpares: ", impares, "\n")
+pares = list(range(2, 20, 2))
+print("Pares ", pares)
+
+
+
+
