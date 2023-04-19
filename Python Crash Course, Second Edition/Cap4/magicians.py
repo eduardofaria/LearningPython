4aadc41b0925065e3a74ee535c181b747b2596fc
Eduardo Faria <eduardofaria.rj@gmail.com>
2023-04-19 09:41:28 -0300

    Prinmeiro exercício com o ambiente montado!

---
+++ Python Crash Course, Second Edition/Cap4/magicians.py
@@ -0,0 +1,7 @@
+magos = ['merlin', 'gandalf', 'leonel', 'mestre dos magos']
+for mago in magos:
+    print(f"{mago.title()}, fireball!")
+    print(f"Errou, {mago.title()}!\n")
+
+print("\n\n")
+print("Todos erraram, valeu, fui!")
